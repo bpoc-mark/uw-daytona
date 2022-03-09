0d69bcd94122ec518595835586ae7896589662c3
bpoc-mark <markariel.maata@bpoc.co.jp>
2022-03-09 17:19:57 +0800

    added story2005 and story2016 page pc and sp view

---
+++ story2016/index.php
@@ -0,0 +1,141 @@
+<?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/config.php"); ?>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/tag/tag_manager.php"); ?>
+  <meta charset="UTF-8" />
+  <meta name="robots" content="index,follow">
+  <link rel="canonical" href="<?php echo CANONICAL ?>">
+  <meta name="keywords" content="<?php echo KEY_WORD ?>">
+  <meta name="description" content="<?php echo DESCRIPTION ?>">
+  <meta property="og:title" content="<?php echo SITE_NAME ?>">
+  <meta property="og:type" content="article" /> <!-- 下層では「article」にしてください -->
+  <meta property="og:description" content="<?php echo DESCRIPTION ?>" />
+  <meta property="og:url" content="<?php echo CANONICAL ?>" />
+
+  <title><?php echo SITE_NAME ?></title>
+
+  <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/header_include.php"); ?>
+  <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/ga.php"); ?>
+  <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/tag/header_tag.php"); ?>
+</head>
+
+<body id="Story">
+  <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/tag/content_tag.php"); ?>
+  <div id="AllBox">
+    <header>
+      <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/header_box.php"); ?>
+    </header>
+
+    <main>
+      <section class="p-head p-head--story">
+        <div class="p-head--wrap">
+          <div class="p-head--content">
+            <h1 class="p-head--content--en">DAYTONA<br>50th STORY</h1>
+            <p class="p-head--content--jp">デイトナ50年の物語</p>
+          </div>
+        </div>
+      </section>
+      <section class="story-sect">
+        <div class="story-lower">
+          <div class="container">
+            <div class="story-lower--item">
+              <h2>常にライダーの目線で<br>「新しい何か」を模索し続ける。</h2>
+              <span class="year">2016-</span>
+              <ul class="list">
+                <li class="list--item">
+                  <h3 class="ttl">末永くバイクライフを、<br class="sp">楽しんでもらうために。</h3>
+                  <p class="desc">
+                    2016年、11年間会社の舵取りをしてきた鈴木から指名され、長年開発現場を担当してきた織田哲司が3代目社長に就任した。<br>
+                    リーマンショック後、組織変更で開発から間接部門に移動した織田はこう振り返る。「リーマンショックから３年間、業績を立て直すために会社は大変な時期だった。私は間接的な部署で、会社のコアコンピタンス（圧倒的な強み）を強化する仕事につき、デイトナと業界を客観的にみる時間ができた。ライダーの集まる場所に通い、ライダーやバイクを観察。話を聞いていくうちに、なんとなく今後の方向性が見えるようになった」。間接部門での３年間が、その後の織田の考え方に大きな影響を与えた。2010年に再び二輪事業部の仕事に戻り、2012年二輪事業部長を経ての代表取締役就任だった。<br>
+                    現在、第３次バイクブームと言われるバイク業界だが、織田は論理的で冷静な見方をする。「10年ほど前はバイク人口の減少が話題だった。バイクの登録台数をみると、減少しているのは原付２輪が多いことに気づく。小・中・大型バイクの登録台数はそれほど変わっていない。今バイク購入の中心層は55歳だが、20代の若いライダーも着実に増えている。さらに数年はリターンライダーが増えるだろう。今後も増えるライダーたちが、ずっとバイクライフを楽しんでもらうために、デイトナができることはまだまだあると思っている」。
+                  </p>
+                  <div class="row">
+                    <img src="/images/story/2016/img-1.jpg" alt="末永くバイクライフを、楽しんでもらうために。">
+                  </div>
+                  <p class="desc">バイクを愛し、社長になった今でもバイク通勤する織田社長。</p>
+                </li>
+                <li class="list--item">
+                  <h3 class="ttl">キャンプすることで、<br class="sp">わかったことがたくさんある。</h3>
+                  <p class="desc">
+                    2018年、デイトナはツーリングテント・ST-IIを商品化。老舗のテントメーカー、キャンパルジャパンと共同開発した。<br>
+                    きっかけはソロキャンプを趣味と公言し、年中キャンプツーリングを楽しむ織田の発案。10年ほど前、新たに道具を揃えようと首都圏でも有数のアウトドアショップを訪れた。ショップスタッフに「バイクに積める大きさのテントは？」と尋ね、これならと勧められた商品を購入。いざバイクに積もうとすると、ケースから数センチはみ出て積めないという経験をした。さらに自身でキャンプを楽しみながら、２輪と４輪のユーザー比率を観察したり、キャンプ道具の使い勝手を実際に身をもって検証した。「今はキャンプ道具が市場に山ほど溢れている。キャンプ経験のある自分でも、何を選べば良いか迷ってしまうほど。キャンプ初心者のライダーは尚更困るだろう。だったら、デイトナがツーリング向けのキャンプ道具を作れば、ライダーから喜ばれるのでは」と考えた。すぐに開発の指示を出したが、１年間は動きがなかった。やはり自分が主体的に動かねばと、自身でメーカーに連絡を取り、ツーリングテント開発の段取りをつけた。バイク業界では先駆けの取り組みとなり、さまざまな商品の開発を見据え「デイトナアウトドアサプライ」としてブランド化した。<br>
+                    一口にキャンプと言っても、４輪のファミリー層とライダーでは、道具に求める機能が変わってくる。デイトナ社内には織田以外にもキャンプを楽しむ社員が多数いる。社員たちは実際にキャンプツーリングでテーブルと椅子の高さや角度を細かく検討。さらに背もたれの高さもハイバックからミドルバックへと改良したり、積載性と拡張性を選べるシステムバックの開発など、ライダー専用と言える商品群へと発展させた。
+                  </p>
+                  <div class="row">
+                    <div class="col-2">
+                      <img src="/images/story/2016/img-2.jpg" alt="キャンプすることで、わかったことがたくさんある。">
+                    </div>
+                    <div class="col-2">
+                      <img src="/images/story/2016/img-3.jpg" alt="キャンプすることで、わかったことがたくさんある。">
+                    </div>
+                  </div>
+                  <p class="desc mgt-10">ツーリングキャンプ用品「デイトナアウトドアサプライ」などバイクパーツメーカーという枠を広げ、ライフスタイルを豊かにする新しい取り組みを続けている。</p>
+                </li>
+                <li class="list--item">
+                  <h3 class="ttl">ここからまた、<br class="sp">挑戦の歴史が始まる。</h3>
+                  <p class="desc">
+                    近年デイトナは二輪パーツ開発という枠組みを超えて、時代の変化を見据えながら、さまざまな領域に挑戦の場を広げている。<br>
+                    中古バイクパーツ売買のスマートフォンアプリ「ブンブン！マーケット」。小型汎用エンジンを活用した「除雪機事業」。リンク式サスペンションを備えた「耕運機DC2S」などが、その代表である。<br>
+                    「ブンブン！マーケット」は、社内プロジェクトの中から若手社員の発案を新規事業として採用した。「発案を事業化する過程では、社外のノウハウも活用。７割程度の確証を得てGOサインを出した。新規事業はやってみないとわからないこともある。途中の軌道修正は想定内ぐらいの気持ちだったが、今は軌道に乗りつつある。若手社員が主体的に動き、結果を出してくれたこと。これが何より嬉しく、大きな財産だと思っている」。<br>
+                    2021年には「耕運機DC2S」がグッドデザイン賞を受賞。これも耕運機という新たなジャンルへの挑戦を大きく後押しする推進力となるはずである。織田が経営で重視する「企業が永続発展するために、今、何にチャレンジしておくべきか？」という問いかけに、確実に社内は答えを出し続けている。
+                  </p>
+                  <div class="row">
+                    <div class="col-2">
+                      <img src="/images/story/2016/img-4.jpg" alt="グッドデザイン賞認定の耕運機「DC2S」">
+                      <p>グッドデザイン賞認定の耕運機「DC2S」</p>
+                    </div>
+                    <div class="col-2">
+                      <img src="/images/story/2016/img-5.jpg" alt="中古バイクパーツの販売アプリ「ブンブンマーケット」">
+                      <p>中古バイクパーツの販売アプリ「ブンブンマーケット」</p>
+                    </div>
+                    <div class="col-2">
+                      <img src="/images/story/2016/img-6.jpg" alt="歴代社長同様、社内を積極的に周り、社員の声に耳を傾ける。">
+                      <p>歴代社長同様、社内を積極的に周り、社員の声に耳を傾ける。</p>
+                    </div>
+                    <div class="col-2">
+                      <img src="/images/story/2016/img-7.jpg" alt="ダートフリークを子会社化、相互補完で新たな展開を模索する。">
+                      <p>ダートフリークを子会社化、相互補完で新たな展開を模索する。</p>
+                    </div>
+                  </div>
+                </li>
+                <li class="list--item">
+                  <h3 class="ttl">バイクを愛するすべての人に。</h3>
+                  <p class="desc">
+                    2022年に創業50周年を迎えるデイトナ。大きな節目を迎えるタイミングで今後の展望を織田はこう語る。「50年後のことは私も分からないが、今言えることは、今後も二輪事業を主軸としてライダーに末長くバイクライフを楽しんでもらえるよう、時代にマッチした新商品開発を続けるということ。さらに事業分野を「趣味」や「生きがい」まで広げ、二輪事業に匹敵するような柱を作るつもりである。そのための布石を色々と打っている。デイトナは、自分で考えて行動する社員とともに、ずっと前を向いて進んでいきたい」。<br>
+                    これからもデイトナはバイクを愛するすべてのライダーとともに、未来へ向けて走り続けていく。
+                  </p>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="profile">
+            <div class="row">
+              <div class="col">
+                <picture >
+                  <source srcset="/images/story/2016/oda-sp.jpg" media="(max-width: 899px)">
+                  <img src="/images/story/2016/oda.jpg" alt="Tetsuji Oda">
+                </picture>
+              </div>
+              <div class="col">
+                <h4 class="suzuki oda">Tetsuji<br>Oda</h4>
+                <p class="top">現・代表取締役社長</p>
+                <h5><span>織田哲司</span></h5>
+                <p class="bottom">1961年生まれ、長年開発の現場を担当し、間接部門を経て2016年に第3代社長に就任。「すぐやる」ことを信条とし、社員とともにデイトナの更なる発展に注力する。可能であればバイクで通勤し、休みはキャンプツーリングと充実したバイクライフを自ら実践している。</p>
+              </div>
+            </div>
+          </div>
+          <a href="" class="reverse">2005-2015</a>
+        </div>
+      </section>
+    </main>
+
+    <footer>
+      <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/footer_box.php"); ?>
+    </footer>
+  </div><!-- //AllBox -->
+  <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/tag/footer_tag.php"); ?>
+</body>
+
+</html>
